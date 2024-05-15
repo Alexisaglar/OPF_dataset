5fad893509fa690416e173cae8990d088e1cf04f
alexys <alexisaglar@gmail.com>
2024-05-15 17:20:43 +0100

    random radial network generator

--- src/generate_data.py
+++ src/generate_data.py
@@ -4,42 +4,44 @@ import pandapower as pp
 import pandapower.networks as nw
 import networkx as nx
 import matplotlib.pyplot as plt
-
-net = nw.case33bw()
-
+import copy
 
 def run_opf(network):
     pp.runopp(network, verbose=False, numba=False)  # Run the optimal power flow
     return network.res_bus, network.res_line
 
 def save_data(bus_data, line_data):
-    data = np.hstack((bus_data.to_numpy(), line_data.to_numpy()))
+    # data = np.hstack((bus_data.to_numpy(), line_data.to_numpy()))
     # data.tonumpy()
-    data.save('dataset_33bus')
+    # data.save('dataset_33bus')
     return
 
-def is_radial(incidence_matrix):
-    n = incidence_matrix.shape[0]
+
+def matrix_rank(switch_matrix, incidence_matrix):
+    total_nodes = len(net.bus.index)
+
+    # Calculate incidence matrix - switch Matrix dot product
+    AS_matrix = np.dot(incidence_matrix, switch_matrix)
     # Calculate rank of the matrix
-    rank = np.linalg.matrix_rank(incidence_matrix)
+    rank = np.linalg.matrix_rank(AS_matrix)
     print(rank)
-    if rank == n - 1:
+    if rank == total_nodes - 1:
+        print('All nodes are connected')
         return True
 
-    return False
+    else:
+        print('Not all nodes are connected')
+        return False
 
-def self_loop(net):
-    adjacency_matrix = np.zeros((len(net.bus.index), len(net.bus.index)))
-    for _, lines in net.line.iterrows():
-        f, t = lines['from_bus'], lines['to_bus']
-        adjacency_matrix[f-1][t-1] = 1
-        adjacency_matrix[t-1][f-1] = 1
-    # determine if the matrix has loops
-    trace = np.trace(adjacency_matrix)
-    if trace > 0:
+def matrix_trace(switch_matrix):
+    total_nodes = len(net.bus.index)
+    trace = np.trace(switch_matrix)
+    if trace == total_nodes - 1:
+        print('Network is radial')       
         return True
-
-    return False
+    else:
+        print('Network is NOT radial')       
+        return False
 
 
 def plot_graph_network(net):
@@ -51,41 +53,47 @@ def plot_graph_network(net):
     plt.show()
     return
 
-
-
-def get_incidence_matrix(net):
-    nodes = net.bus.index
-    lines = net.line
+def incidence_matrix(net):
+    new_net = copy.deepcopy(net)
+    mask = ~new_net.line['in_service']
+    new_net.line.loc[mask, 'in_service'] = True
+    lines = new_net.line
+    nodes = new_net.bus.index
     
     # Initialize adjacency matrix with zeros
     incidence_matrix = np.zeros((len(nodes), len(lines)))
 
     # Add edges from the lines in the network
     for idx, line in net.line.iterrows():
-        incidence_matrix[line.from_bus, idx] = 1
-        incidence_matrix[line.to_bus, idx] = -1
+        incidence_matrix[line.from_bus, idx] = -1
+        incidence_matrix[line.to_bus, idx] = 1
 
     return incidence_matrix
 
-def new_network_connection(net):
-    for i in range(10):
-        original_state = net.line.copy()
-        mask = np.where(net.line['in_service'] == False)
-        net.line['in_service'].iloc[mask] = True
-        selected_nodes = np.random.choice(net.line.index[1:], size=5)
-        net.line['in_service'].iloc[selected_nodes] = False
-        plot_graph_network(net)
-        # if not self_loop(net) and is_radial(incidence_matrix):
-        #     x = np.append(np.array(net.line['in_service']))
+def service_matrix(net):
+    switch_matrix = np.zeros((len(net.line), len(net.line)))
+    in_service = net.line['in_service']
+    np.fill_diagonal(switch_matrix, in_service)
 
+    return switch_matrix
+
+def new_network_configuration(net):
+    selected_nodes = np.random.choice(net.line.index[1:], size=5)
+    mask = ~net.line['in_service']
+    net.line.loc[mask, 'in_service'] = True
+    net.line.loc[selected_nodes, 'in_service'] = False
+    
     return net
 
 if __name__ == '__main__':
-    incidence_matrix = get_incidence_matrix(net)
-    # switch_matrix = switch_matrix(net)
-
-    new_network_connection(net)
-    
-    if not self_loop(net) and is_radial(incidence_matrix):
-        bus_data, line_data = run_opf(net)
-        plot_graph_network(net)
+    net = nw.case33bw()
+    switch_matrix = service_matrix(net)
+    incidence_matrix = incidence_matrix(net)
+    for i in range(1000):
+        net = new_network_configuration(net)
+        print(net.line['in_service'])
+        switch_matrix = service_matrix(net)
+        if matrix_rank(switch_matrix, incidence_matrix):
+            if matrix_trace(switch_matrix):
+                plot_graph_network(net)
+        
